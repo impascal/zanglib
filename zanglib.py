c8e9eced7e284bd165ad44e9ccade94a0032a7b7
pascal <45119870+impascal@users.noreply.github.com>
2026-03-30 12:40:33 +0200

    Add LR gauss factorization (diagonal strategy)

--- zanglib.py
+++ zanglib.py
@@ -83,39 +83,44 @@ def check_and_test():
         print(f"{test:12}: {'PASS' if status else 'FAIL'}")
 
 
-def utrisol(R: np._ArrayFloat64_co, b: np._ArrayFloat64_co) -> np._ArrayFloat64_co:
+def utrisol(R: np.ndarray, b: np.ndarray):
     """
-    Solve linear system Rx = b in which R is an upper triangular matrix
-    and b is the vector of constant terms using backward substution algorithm.
-    Both R and b are not preserved, b can be used both as return value
-    and output parameter.
+    Solve linear system Rx = b using backward subtitution
+    PARAMETERS:
+        R: Numpy upper triangular matrix
+        b: Numpy 1D array
     """
-    [n, m] = R.shape
-    if (n != m) or len(R.shape) > 2:
+
+    if not isinstance(R, np.ndarray):
+        raise TypeError("R must be a Numpy ndarray...")
+
+    if not isinstance(b, np.ndarray):
+        raise TypeError("b must be a NumPy array...") 
+
+    if len(R.shape) > 2 or R.shape[0] != R.shape[1]:
         raise ValueError("R must be a n x n upper triangular matrix...")
 
     if len(b.shape) > 1:
-        raise ValueError("b must be a one dimensional array...") 
+        raise ValueError("b must be a one dimensional array...")
 
-    if len(b) != n:
+    if b.shape[0] != R.shape[0]:
         raise ValueError("b must be and R must have the same number of rows...")
 
     # Convert everything to float64 just to be sure
     np.float64(R)
     np.float64(b)
 
-    eps = np.finfo(np.float64).eps * norm(R, np.inf)
-    if any(np.diag(R) < eps):
+    eps = np.finfo(np.float64).eps
+    if any(np.abs(np.diag(R)) < eps):
         raise ValueError("Some value of R are numerically too little...")
 
-    for i in range(n - 1, -1, -1):
+    for i in range(R.shape[0] - 1, -1, -1):
         b[i] = b[i] / R[i, i]
         R[0:i, i] *= b[i]
         b[0:i] -= R[0:i, i]
 
-    return b
 
-def ltrisol(L: np._ArrayFloat64_co, b: np._ArrayFloat64_co) -> np._ArrayFloat64_co:
+def ltrisol(L: np.ndarray, b: np.ndarray):
     """
     Solve linear system Lx = b in which R is a lower triangular matrix
     and b is the vector of constant terms using forward substution algorithm.
@@ -128,7 +133,7 @@ def ltrisol(L: np._ArrayFloat64_co, b: np._ArrayFloat64_co) -> np._ArrayFloat64_
         raise ValueError("R must be a n x n upper triangular matrix...")
 
     if len(b.shape) > 1:
-        raise ValueError("b must be a one dimensional array...") 
+        raise ValueError("b must be a one dimensional array...")
 
     if len(b) != n:
         raise ValueError("b must be and R must have the same number of rows...")
@@ -137,13 +142,54 @@ def ltrisol(L: np._ArrayFloat64_co, b: np._ArrayFloat64_co) -> np._ArrayFloat64_
     np.float64(L)
     np.float64(b)
 
-    eps = np.finfo(np.float64).eps * norm(L, np.inf)
+    eps = np.finfo(np.float64).eps
     if any(np.diag(L) < eps):
         raise ValueError("Some value of R are numerically too little...")
 
     for i in range(n):
         b[i] = b[i] / L[i, i]
-        L[i+1:n, i] *= b[i]
-        b[i+1:n] -= L[i+1:n, i]
+        L[i + 1 : n, i] *= b[i]
+        b[i + 1 : n] -= L[i + 1 : n, i]
 
-    return b
+def gaussDiag(A: np.ndarray, b: np.ndarray) -> int:
+    """
+    In-place LR factorization of A using Gauss elimination algorithm
+    with diagonal strategy. Solution (if unique) will be placed in b
+
+    Returns the number of steps the algorithm achieved before stopping,
+    if steps == A.shape[0] a solution has been found
+    """
+    n = A.shape[0]
+    
+    np.float64(A)
+    np.float64(b)
+
+    eps = np.finfo(np.float64).eps
+
+    I = np.eye(n, n)
+
+    for i in range(n - 1):
+        if np.abs(A[i,i]) < eps:
+            return i
+
+        m = A[i+1:n, i] / A[i,i] # array dei moltiplicatori 
+
+        # trasformazione elementare di gauss compatta 
+        # (assegno ad una identità, sotto la diagonale, nella colonna i-esima i moltiplicatori
+        # con segno cambiato
+        L = np.eye(n, dtype=np.float64)
+        L[i+1:n, i] = - m
+
+        # applico la trasformazione elementare 
+        np.matmul(L, A, out=A)
+        np.matmul(L, b, out=b)
+
+        # nella colonna i-esima inserisco la rispettiva colonna della fattorizzazione
+        # calcolata come l'inverso della trasformazione elementare
+        A[i+1:n, i] = m
+
+    if np.abs(A[n-1,n-1]) < eps:
+        return n - 1
+
+    utrisol(np.triu(A), b)
+    return n
